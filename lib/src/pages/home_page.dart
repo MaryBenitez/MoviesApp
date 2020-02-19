71af5d3a02eaf5ea7d138d430fe33bfe5d042804
MaryBenitez <marisol.dominguezxp@gmail.com>
2020-02-19 14:38:49 -0600

    Widget personalizado, muestra peliculas populares en la parte inferior, la vista se ve mas agradable, aun hay un problema en la primera posicion del scroll

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
   final peliculasProviders = new PeliculasProviders();
@@ -8,28 +9,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black26,
-      appBar: AppBar(
-        title: Text('Peliculas en Cine'),
-        backgroundColor: Colors.blueGrey,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search), 
-            onPressed: () {}
-          ),
-        ],
-      ),
-      body:
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Peliculas en Cine'),
+          backgroundColor: Colors.black,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {}),
+          ],
+        ),
+        body:
             //"SafeArea" Widget que respeta la muesca del celular
-        Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              _swiperTarjeta(),
-              _footer(context),
-            ],
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                _swiperTarjeta(),
+                _footer(context),
+              ],
+            )        
           )
-        )
     );
   }
 
@@ -53,14 +51,19 @@ class HomePage extends StatelessWidget {
     return Container(
       width: double.infinity,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text('Populares', style: Theme.of(context).textTheme.subhead),
+          Container(
+            padding: EdgeInsets.only(left:20.0, bottom: 10.0, top: 15.0),
+            child: Text('Populares', style: Theme.of(context).textTheme.subhead)
+          ),
           FutureBuilder(
             future: peliculasProviders.getPopulares(),
             builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-              snapshot.data.forEach((p) => print(p.title)
-            );
-              return Container();
+              if (snapshot.hasData) {
+                return MovieHorizontal(peliculas: snapshot.data);
+              }
+              return CircularProgressIndicator();
             },
           ),
         ],
